64361721fdc1a693810e576f5447a9a76840813f
Julius Torralba <jtorralba@eptc911.org>
2023-10-03 14:08:43 -0600

    Optimize code.

--- CAD/X.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace CAD
-{
-    public class X
-    {
-
-    }
-}
--- Service/Service.Designer.cs
+++ Service/Service.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Service
 {
-    partial class Service1
+    partial class Service
     {
         /// <summary> 
         /// Required designer variable.
